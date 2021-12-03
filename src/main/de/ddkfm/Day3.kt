6d3d96c9ec2e19f302ac1165dd0f59b4202e5bb0
DDKFM <dehilnor@ddkfm.de>
2021-12-03 18:55:31 +0100

    Refactoring, lol

--- src/main/de/ddkfm/Day3.kt
+++ src/main/de/ddkfm/Day3.kt
@@ -2,22 +2,16 @@ package de.ddkfm
 
 class Day3 : DayInterface<List<String>, Int> {
     override fun part1(input: List<String>): Int {
-        val inverted = mutableListOf<String>()
-        input.forEach { row ->
-            for(i in row.indices) {
-                if(inverted.size > i)
-                    inverted[i] += row[i].toString()
-                else
-                    inverted.add(row[i].toString())
-            }
+        val maxLength = input.maxOf { it.length }
+        val rates = (0 until maxLength).map { i ->
+            var recognizableChars = input.map { it[i] }
+            val mostCommonBit = getMostCommonBits(recognizableChars)
+            val leastCommonBit = getLeastCommonBits(recognizableChars)
+            return@map mostCommonBit to leastCommonBit
         }
-        println(inverted)
-        val sums = inverted.map { it.groupBy { it }.mapValues { it.value.size } }
-        println(sums)
-        val gamma = sums.map { it.maxByOrNull { it.value } }.map{ it?.key }.joinToString(separator = "")
-        val epsilon = sums.map { it.minByOrNull { it.value }}.map{ it?.key }.joinToString(separator = "")
-        println("$gamma, $epsilon")
-        return gamma.toInt(2) * epsilon.toInt(2)
+        val gamma = rates.joinToString(separator = "") { it.first.toString() }.toInt(2)
+        val epsilon = rates.joinToString(separator = "") { it.second.toString() }.toInt(2)
+        return gamma * epsilon
     }
 
     override fun part2(input: List<String>): Int {
@@ -35,8 +29,8 @@ class Day3 : DayInterface<List<String>, Int> {
         i = 0;
         while(remaining.size > 1) {
             var recognizableChars = remaining.map { it[i] }
-            val mostCommonBit = getLeastCommonBits(recognizableChars)
-            remaining = remaining.filter { it[i] == mostCommonBit }.toMutableList()
+            val lestCommonBit = getLeastCommonBits(recognizableChars)
+            remaining = remaining.filter { it[i] == lestCommonBit }.toMutableList()
             i++
         }
         val co2Rating = remaining.first().toInt(2)
